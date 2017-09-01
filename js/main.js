8f9669e95d84e3df5f70c8db4b8ccc26691d2102
Andrew Kornienko <kornienko.andriy@gmail.com>
2017-09-01 15:41:29 +0300

    add decorative header

--- js/main.js
+++ js/main.js
@@ -20,5 +20,21 @@ $(document).ready(function () {
     });
 
 
+    maintainSameHeight($('[data-same-height="testimonial"]'));
+
+    function maintainSameHeight($list) {
+        var height = 0;
+
+        $list.each(function () {
+            var $this = $(this);
+            if ($this.outerHeight() > height) {
+                height = $this.outerHeight();
+            }
+        });
+
+        $list.css("height", height);
+    }
+
+
 
 });
